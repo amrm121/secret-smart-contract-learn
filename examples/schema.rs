3bc68580ee9968569c9823519b980e00d1cf6945
Alexandre Magalhães <amrm@cin.ufpe.br>
2022-06-01 21:56:07 -0300

    generating schemas

--- examples/schema.rs
+++ examples/schema.rs
@@ -3,8 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use secret_contract_example::msg::{CountResponse, HandleMsg, InitMsg, QueryMsg};
-use secret_contract_example::state::State;
+use secret_contract_learning::msg::{CountResponse, HandleMsg, InitMsg, QueryMsg};
+use secret_contract_learning::state::State;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
